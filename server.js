a0cb99984643344e69f3a064775cc5e7be2a34c8
Neon Studio <s11331136@nhsh.tp.edu.tw>
2026-09-03 00:46:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ io.on('connection', (socket) => {
             socket.broadcast.emit('playerMoved', { id: socket.id, data: data });
         }
     });
+    // 監聽玩家加入指定房間
+    socket.on('join1v1', (roomId) => {
+        socket.join(roomId);
+        
+        // 取得目前房間內的連線人數
+        const room = io.sockets.adapter.rooms.get(roomId);
+        const numClients = room ? room.size : 0;
+    
+        // 當房間達到 2 人時，通知雙方遊戲開始，且不生成 Bot
+        if (numClients === 2) {
+            io.to(roomId).emit('matchReady', { room: roomId });
+        }
+    });
 
     // 斷線處理
     socket.on('disconnect', () => {
