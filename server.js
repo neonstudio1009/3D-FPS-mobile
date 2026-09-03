c8fadfa5c7175ae4febd94f37b3cb7791f58dd05
Neon Studio <s11331136@nhsh.tp.edu.tw>
2026-09-03 17:59:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,31 @@ io.on('connection', (socket) => {
     // 廣播給其他人有新玩家加入
     socket.broadcast.emit('newPlayer', { id: socket.id });
 
+    // 連線時預設血量 100
+    players[socket.id] = { x: 0, y: 1.7, z: 0, rotationY: 0, hp: 100 };
+    
+    // 1. 廣播開槍動作（讓其他人看到開槍效果）
+    socket.on('playerShoot', (shootData) => {
+        socket.broadcast.emit('enemyShoot', { id: socket.id, ...shootData });
+    });
+    
+    // 2. 接收傷害判定並廣播扣血
+    socket.on('hitPlayer', (data) => {
+        const target = players[data.targetId];
+        if (target) {
+            target.hp -= data.damage;
+    
+            // 廣播血量更新給所有人
+            io.emit('playerDamaged', { id: data.targetId, hp: target.hp });
+    
+            // 判定死亡與重生
+            if (target.hp <= 0) {
+                target.hp = 100; // 重置血量
+                io.emit('playerKilled', { victimId: data.targetId, killerId: socket.id });
+            }
+        }
+    });
+    
     // 處理玩家位移
     socket.on('playerMove', (data) => {
         if (players[socket.id]) {
@@ -46,6 +71,7 @@ io.on('connection', (socket) => {
         delete players[socket.id];
         io.emit('playerLeft', socket.id);
     });
+    
 });
 
 // 使用 Render 提供動態 Port 或預設 3000
